2cd7371a8b2bf09066848dd026fac344ef5e7306
¨mago¨ <martingramajo08@gmail.com>
2023-07-11 21:52:16 -0300

    agrego titulo y el footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,20 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1>hello React</h1>
-    </>
+    <div className="d-flex flex-column min-vh-100 color-gradiente-bg">
+      <section className="container">
+        <h1 className="display-5 text-center pt-5 pb-2 text-white">
+          Lista de tareas
+        </h1>
+        <hr className="text-white" />
+      </section>
+      <footer className="py-5 mt-auto bg-dark">
+        <h5 className="text-center text-white">
+          {" "}
+          © Todos los derechos reservados
+        </h5>
+      </footer>
+    </div>
   );
 }
 
